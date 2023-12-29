cd1fc9264c8c06a3f14b1ce5b4743faa81741b6f
Felipe Mandiola <hi@icarusmind.com>
2023-12-29 00:27:25 -0300

    Quick fix

--- src/components/TrackInfo/index.tsx
+++ src/components/TrackInfo/index.tsx
@@ -1,17 +1,6 @@
-import React from 'react';
-
-interface TrackInfoProps {
-  trackName: string;
-  artistName: string;
-  albumName: string;
-}
-
-const TrackInfo: React.FC<TrackInfoProps> = ({ trackName, artistName, albumName }) => {
+const TrackInfo = () => {
   return (
     <div>
-      <h2>{trackName}</h2>
-      <p>{artistName}</p>
-      <p>{albumName}</p>
     </div>
   );
 };
